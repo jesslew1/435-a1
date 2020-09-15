538025f452d983f067255f265078159b0b51a1e6
jesslew1 <46565277+jesslew1@users.noreply.github.com>
2020-09-15 12:27:09 -0400

    Add files via upload

--- a1.py
+++ a1.py
@@ -3,7 +3,6 @@ def main ():
 
     # open and read sample.txt file from argument; 'r' reads the file
     text = open(sys.argv[1], 'r')
-    
     # input duplicates into 2 dictionaries; use one unordered dictionary for duplicate occurances and the other to count the number of occurances
     duplicates = dict()
     counter  = dict()
@@ -12,19 +11,16 @@ def main ():
     for line in text:
         #remove trailing and leading string characters
         numb = line.strip()
-        
         #convert string to an integer with base 16 hex
         a = int(numb, base=16)
-
         #if length of string is greater than 1, string the 0s
         if len(numb)>1:
             numb = numb.lstrip('0')
-
         #increment the number of occurances and put in collections
         #index the stripped string and index the number of occurances
         duplicates[a] = numb
         counter[a] = counter.get(a,0) +1
-
+        
     #use keys for valued pairs and then sort dictionaries from from the smallest value to the largest value in hex
     keys = sorted(counter.keys(), cmp=None, key=None, reverse=False)
 
